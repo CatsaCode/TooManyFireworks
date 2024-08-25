712bfe669d3bb1e83f410bb883be5b54a44e1e05
CatsaCode <catsacode@gmail.com>
2024-08-25 00:01:21 -0400

    Set PLAY text to YEET!
    Add the demo code from https://bsmg.wiki/modding/quest-mod-dev-intro.html and experiment with the hook used to make it run for every map selection instead of just the first.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "main.hpp"
 
+#include "GlobalNamespace/StandardLevelDetailView.hpp"
+#include "GlobalNamespace/StandardLevelDetailViewController.hpp"
+#include "UnityEngine/UI/Button.hpp"
+#include "UnityEngine/GameObject.hpp"
+#include "HMUI/CurvedTextMeshPro.hpp"
+
 #include "scotland2/shared/modloader.h"
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
@@ -12,6 +18,35 @@ static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 Configuration &getConfig() {
   static Configuration config(modInfo);
   return config;
+}
+
+// Create a hook struct named LevelUIHook
+// targeting the method "StandardLevelDetailViewController::DidActivate", which takes the following arguments:
+// bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling
+// and returns void.
+
+// General format: MAKE_HOOK_MATCH(hook name, hooked method, method return type, method class pointer, arguments...) {
+//     HookName(self, arguments...);
+//     your code here
+// }
+
+
+MAKE_HOOK_MATCH(LevelUIHook, &GlobalNamespace::StandardLevelDetailViewController::ShowContent, void,
+ GlobalNamespace::StandardLevelDetailViewController* self, ::GlobalNamespace::__StandardLevelDetailViewController__ContentType contentType, ::StringW errorText) {
+    // Run the original method before our code.
+    // Note that you can run the original method after our code or even in the middle
+    // if you want to change arguments or do something before it runs.
+    LevelUIHook(self, contentType, errorText);
+
+    // Get the actionButton text object by accessing the actionButton field and some simple Unity methods.
+    // Note that auto can be used instead of declaring the full type in many cases.
+    GlobalNamespace::StandardLevelDetailView* standardLevelDetailView = self->_standardLevelDetailView;
+    UnityEngine::UI::Button* actionButton = standardLevelDetailView->actionButton;
+    UnityEngine::GameObject* gameObject = actionButton->get_gameObject();
+    HMUI::CurvedTextMeshPro* actionButtonText = gameObject->GetComponentInChildren<HMUI::CurvedTextMeshPro*>();
+
+    // Set the text
+    actionButtonText->set_text("YEET!");
 }
 
 // Called at the early stages of game loading
@@ -31,6 +66,8 @@ MOD_EXTERN_FUNC void late_load() noexcept {
   il2cpp_functions::Init();
 
   PaperLogger.info("Installing hooks...");
+
+  INSTALL_HOOK(PaperLogger, LevelUIHook);
 
   PaperLogger.info("Installed all hooks!");
 }
