09502690a9272c6dbbd38b8d354f955859d6b6d1
CatsaCode <catsacode@gmail.com>
2025-07-02 23:35:23 -0400

    Fix enableInLevelSelection not updating fireworks during pause to menu transition
    Refactored levelSelection.cpp to use a skipNextActivation boolean and a few hooks to track its state.
    This bug is very very minor and probably nobody cares but I noticed it so I'm fixing it anyway.

--- src/ForceEnable/levelSelection.cpp
+++ src/ForceEnable/levelSelection.cpp
@@ -2,39 +2,84 @@
 
 #include "fireworkManager.hpp"
 
-#include "GlobalNamespace/SelectLevelCategoryViewController.hpp"
+#include "GlobalNamespace/AudioTimeSyncController.hpp"
+#include "GlobalNamespace/ResultsViewController.hpp"
+#include "GlobalNamespace/PauseMenuManager.hpp"
+
+#include "GlobalNamespace/LevelCollectionNavigationController.hpp"
 
 using namespace GlobalNamespace;
 using namespace UnityEngine;
 
 namespace TooManyFireworks {
 
+    // Whether or not to skip toggling fireworks in the next call to LevelCollectionNavigationController::DidActivate
+    bool skipNextActivation = false;
+    
+    // Skip next call when level is failed or finished
+    MAKE_HOOK_MATCH(
+        DetectLevelStartHook,
+        &AudioTimeSyncController::StartSong,
+        void,
+        AudioTimeSyncController* self,
+        float startTimeOffset
+    ) {
+        DetectLevelStartHook(self, startTimeOffset);
+        
+        skipNextActivation = true;
+    }
+
+    // Skip next call during transition from results screen to gameplay
+    MAKE_HOOK_MATCH(
+        DetectResultsRestartHook,
+        &ResultsViewController::RestartButtonPressed,
+        void,
+        ResultsViewController* self
+    ) {
+        DetectResultsRestartHook(self);
+        
+        skipNextActivation = true;
+    }
+
+    // The menu button in the pause menu skips the results screen and therefore the second activation, cancel previously setting the flag
+    MAKE_HOOK_MATCH(
+        DetectMenuFromPauseHook,
+        &PauseMenuManager::MenuButtonPressed,
+        void,
+        PauseMenuManager* self
+    ) {
+        DetectMenuFromPauseHook(self);
+        
+        skipNextActivation = false;
+    }
+
     // Hook to enable the fireworks in the menu for selecting levels
     MAKE_HOOK_MATCH(
         ForceEnableLevelSelectionHook,
-        &SelectLevelCategoryViewController::DidActivate,
+        &LevelCollectionNavigationController::DidActivate,
         void,
-        SelectLevelCategoryViewController* self,
+        LevelCollectionNavigationController* self,
         bool firstActivation, 
         bool addedToHierarchy, 
         bool screenSystemEnabling
     ) {
         // Run original function
         ForceEnableLevelSelectionHook(self, firstActivation, addedToHierarchy, screenSystemEnabling);
-
-        // For some reason, this activates when the results screen appears? Then again after closing the results screen? And these seem to be the only two instances where addedToHierarchy is false? Um... sort this weirdness out I guess
-        static bool lastAddedToHierarchy = false;
-        if(lastAddedToHierarchy && !addedToHierarchy) {
-            lastAddedToHierarchy = addedToHierarchy;
+        
+        // The LevelCollectionNavigationController has a few additional situations where it may activate; skip them
+        if(skipNextActivation) {
+            skipNextActivation = false;
             return;
         }
-        lastAddedToHierarchy = addedToHierarchy;
 
         // Enable or disable fireworks
         SetMainFireworksEnabled(getModConfig().enableInLevelSelection.GetValue());
     }
 
     void InstallForceEnableLevelSelectionHook() {
+        INSTALL_HOOK(PaperLogger, DetectLevelStartHook);
+        INSTALL_HOOK(PaperLogger, DetectResultsRestartHook);
+        INSTALL_HOOK(PaperLogger, DetectMenuFromPauseHook);
         INSTALL_HOOK(PaperLogger, ForceEnableLevelSelectionHook);
     }
 
